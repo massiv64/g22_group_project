e2af02ace8945ac2d2ee36c4dec612263c818551
krpeacock <kylpeacock@gmail.com>
2016-04-22 15:56:47 -0700

    old file

--- migrations/20160422112500_create_cat_post.js
+++ migrations/20160422112500_create_cat_post.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('cat_post', function(table){
-    table.bigInteger('user_id').unsigned().index().references('id').inTable('users').onDelete('cascade');
+    table.bigInteger('cat_id').unsigned().index().references('id').inTable('users').onDelete('cascade');
     table.increments();
     table.bigInteger('post_id').unsigned().index().references('id').inTable('posts').onDelete('cascade');
   })
