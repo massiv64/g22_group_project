1fb51b34f1c9ff17ff568f93e405cd995e00f3ba
sarabpj <sjay02@yahoo.com>
2016-04-25 16:47:33 -0700

    fixed nav bar, create test files

--- test/test.main.js
+++ test/test.main.js
@@ -1,5 +1,8 @@
+process.env.NODE_ENV = 'test';
+
 const request = require('supertest');
 const expect = require('chai').expect;
+const knex = require('../db/knex');
 const app = require('../app');
 
 describe('GET /', () => {
