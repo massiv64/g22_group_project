c929f363f9fb5d212a2a7911f749c39846776328
Chris Chang <chrischang43@gmail.com>
2016-04-25 10:21:00 -0700

    adding front page with React

--- routes/main.js
+++ routes/main.js
@@ -8,14 +8,19 @@ router.get("/", (req,res) => {
 });
 
 router.get('/posts', (req,res) => {
-  knex.select("posts.id as post_id", "users.alias", "users.id", "posts.user_id", "posts.body", "posts.title").from('posts').join("users", "posts.user_id", "users.id").then((posts) => {
+  knex.select("posts.id as post_id", "users.alias", "posts.user_id as user_id", "posts.body", "posts.title").from('posts').join("users", "posts.user_id", "users.id").then((posts) => {
     Promise.map(posts, (p) => {
       return knex('comments').where({post_id: p.post_id}).then(function(comments){
         p.comments = comments
         return p;
     })
   }).then(function(posts){
-    res.render("main/all", {posts})
+    res.format({
+      'application/json':() => {
+        res.send(posts)
+      }  
+    })
+    
   })
   }).catch(function(err){
     res.render("error", {err})
