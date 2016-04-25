01f96b571344ae82ea416ff96520b755db2a3a02
krpeacock <kylpeacock@gmail.com>
2016-04-25 13:57:28 -0700

    auth routes mostly in order

--- routes/users.js
+++ routes/users.js
@@ -7,64 +7,23 @@ const bcrypt = require('bcrypt')
 const SALT_WORK_FACTOR = 10
 
 
-//local strategy - nh
-passport.use(new LocalStrategy({
-    usernameField: 'user[email]',
-    passwordField: 'user[passsword]',
-    passReqToCallback: true
-  },
-    function(email, password, next){
-      knex('users').where('email', email).first().then(user => {
-        if (err) { return done(err) }
-          if (!user) {
-            return done(null, false)
-          }
-          if (!bcrypt.compareSync(password, user.password)) {
-            return done(null, flase)
-          }
-          return done(null, user)
-      })
-    }
-));
-
-passport.serializeUser(function(user, done){
-  done(null, user.id)
-});
-
-passport.deserializeUser(function(user, done) {
-  knex('users').where('id', id).first().then(user => {
-    done(null, user)
-  }).catch(err, false);
-});
-
-router.get('/login', function(req, res){
-  res.render('login', {
-    message: loginMessage
-  });
-});
-
-app.get('/auth/google', passport.authenticate('google', {scope:['profile', 'email']}));
-
-app.get('/auth/google/callback',
-  passport.authenticate('google', { successRedirect: '/profile',
-                                      failureRedirect: '/'}))
 
 router.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/users/login');
 }); 
 
-router.get('/', helpers.ensureAuthenticated, (req,res) => {
-  knex('users').then((users) =>{
-    res.render("users/index", {users})
-  }).catch((err) =>{
-    res.render("error", {err})
-  });
-});
-
-router.get('/new',helpers.preventLoginSignup, (req,res) => {
-  res.render("users/new")
-})
+//router.get('/', helpers.ensureAuthenticated, (req,res) => {
+//  knex('users').then((users) =>{
+//    res.render("users/index", {users})
+//  }).catch((err) =>{
+//    res.render("error", {err})
+//  });
+//});
+//
+//router.get('/new',helpers.preventLoginSignup, (req,res) => {
+//  res.render("users/new")
+//})
 
 router.get('/:id', (req,res) => {
   knex('users').where({id: req.params.id}).first().then((user) =>{
