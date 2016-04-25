b88b2d7f741c151a6b1e7663dafd4205de28492d
krpeacock <kylpeacock@gmail.com>
2016-04-25 13:59:43 -0700

    cleaning up paths

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,10 @@ const main = require("./main");
 const comments = require("./comments");
 const posts = require("./posts");
 const users = require("./users");
-const auth = require("./auth")
 
 module.exports = {
   main,
   comments,
   posts,
   users,
-  auth
 }
