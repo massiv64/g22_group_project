fd71f4c3bc0bd57e050563fb8d3eca066faeeaa5
Chris Chang <chrischang43@gmail.com>
2016-04-26 09:27:17 -0700

    adding simple markdown editor

---
+++ public/javascripts/script.js
@@ -0,0 +1,15 @@
+var simplemde = new SimpleMDE({
+  element: document.getElementById("body"),
+  renderingConfig: {
+    singleLineBreaks: false,
+    codeSyntaxHighlighting: true,
+  }
+});
+
+var simplemde = new SimpleMDE({
+  element: document.getElementById("preview"),
+  renderingConfig: {
+    singleLineBreaks: false,
+    codeSyntaxHighlighting: true,
+  }
+});
