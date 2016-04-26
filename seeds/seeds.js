0df714b31603e7aa5a36975a5f812723a0139037
krpeacock <kylpeacock@gmail.com>
2016-04-25 17:11:15 -0700

    knex seeds with a post

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -7,6 +7,9 @@ exports.seed = function(knex, Promise) {
     // Inserts seed entries
     knex('users').insert({id: 2000, alias: "Jenny", photo: 'https://goo.gl/ISKmPE', email: 'testlo@test.com'}),
     knex('users').insert({id: 2001, photo: 'http://goo.gl/Wiw2c', email: 'test@test.com', alias: "Picard"}),
-    knex('users').insert({id: 2002, photo: 'http://goo.gl/JUCPFY', email: 'calvin@abcglobal.edu', alias: 'Calvin'})
+    knex('users').insert({id: 2002, photo: 'http://goo.gl/JUCPFY', email: 'calvin@abcglobal.edu', alias: 'Calvin'}),
+    knex('categories').insert({technology: 'javascript'}), 
+    knex('categories').insert({technology: 'html'}),
+    knex('posts').insert({id: 1000,user_id: 2001, title: "Why I'm so down to Earth and likeable", body: "I don't really know, I just try to stay grounded and, like, keep it real even though I've had so much success!"})
   );
 };
