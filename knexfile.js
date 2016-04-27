df21918067d4b1534952ef6727b7e03c9c4836ca
krpeacock <kylpeacock@gmail.com>
2016-04-27 13:54:33 -0700

    removes connection from knexfile prod

--- knexfile.js
+++ knexfile.js
@@ -30,8 +30,12 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: process.env.DATABASE_URL
+    // connection: {
+    //   database: process.env.DATABASE_URL
+    // },
+    pool: {
+      min: 2,
+      max: 10
     },
     migrations: {
       tableName: 'knex_migrations'
