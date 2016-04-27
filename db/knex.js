f36d0018bb4f3140dc47503989d976639c6c743f
krpeacock <kylpeacock@gmail.com>
2016-04-27 13:27:42 -0700

    knex setup for env

--- db/knex.js
+++ db/knex.js
@@ -1,3 +1,3 @@
-const env = process.env.NODE_ENV || 'development'
+const env = process.env.DATABASE_URL || 'development'
 const config = require("../knexfile")[env];
 module.exports = require('knex')(config);
