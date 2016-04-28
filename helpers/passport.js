557cf15e76f7dd55c7a3b1dc782184b106f14b18
krpeacock <kylpeacock@gmail.com>
2016-04-27 17:33:39 -0700

    configure google routing for env url

--- helpers/passport.js
+++ helpers/passport.js
@@ -11,7 +11,7 @@ module.exports = (passport) => {
   passport.use(new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/google/callback',
+      callbackURL: `${process.env.URL}/auth/google/callback`,
       scope: ['profile', 'email'],
       state: true
   },
