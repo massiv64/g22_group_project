d68868302323eab0872d162642338c264b1f4861
Chris Chang <chrischang43@gmail.com>
2016-04-28 13:29:58 -0700

    testing

--- routes/main.js
+++ routes/main.js
@@ -37,6 +37,30 @@ router.get('/posts', (req,res) => {
   })
 })
 
+router.get('/testing', (req,res) => {
+  // Chris's code
+  knex('posts as p').select('p.id as post_id', 'u.alias', 'p.user_id as user_id', 'p.title', 'p.body', 'cp.category_id', 'c.technology')
+  .join('users as u', 'p.user_id', 'u.id')
+  .join('category_posts as cp', 'p.id', 'cp.post_id')
+  .join('categories as c', 'cp.category_id', 'c.id')
+  .then(posts => {
+    console.log(posts, 'this is before reduce');
+    posts = posts.reduce((prev, next) => {
+      var post = prev.find(post => { return post.post_id === next.post_id} );
+      if (post === undefined) {
+        post = {post_id: next.post_id, alias: next.alias, user_id: next.user_id, title: next.title, body: next.body, categories: []}; 
+        prev.push(post);
+      }
+      post.categories.push({category_id: next.category_id, technology: next.technology});
+      return prev;
+    }, []);
+    console.log(posts, 'this is after reduce')
+    res.send(posts);
+  }).catch(function(err){
+      res.render("error", {err})
+  })
+})
+
 router.get('/categories', (req,res) => {
   knex('categories').then(categories => {
         res.json(categories)
