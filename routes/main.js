d660907e6a2a794e17e018e97fd5a2c00d35cc9a
Chris Chang <chrischang43@gmail.com>
2016-04-28 13:23:49 -0700

    debugging main.js

--- routes/main.js
+++ routes/main.js
@@ -20,7 +20,8 @@ router.get('/posts', (req,res) => {
   .join('category_posts as cp', 'p.id', 'cp.post_id')
   .join('categories as c', 'cp.category_id', 'c.id')
   .then(posts => {
-    var postList = posts.reduce((prev, next) => {
+    console.log(posts, 'this is before reduce');
+    posts = posts.reduce((prev, next) => {
       var post = prev.find(post => { return post.post_id === next.post_id} );
       if (post === undefined) {
         post = {post_id: next.post_id, alias: next.alias, user_id: next.user_id, title: next.title, body: next.body, categories: []}; 
@@ -29,7 +30,8 @@ router.get('/posts', (req,res) => {
       post.categories.push({category_id: next.category_id, technology: next.technology});
       return prev;
     }, []);
-    res.send(postList);
+    console.log(posts, 'this is after reduce')
+    res.send(posts);
   }).catch(function(err){
       res.render("error", {err})
   })
